154bac034233d3f2d853531576736375d5752b7f
SmaugsLair <77090314+SmaugsLair@users.noreply.github.com>
2021-07-13 16:09:54 -0700

    Added freindship

--- src/main/java/com/smaugslair/thitracker/ui/friends/FriendsSession.java
+++ src/main/java/com/smaugslair/thitracker/ui/friends/FriendsSession.java
@@ -1,21 +1,10 @@
 package com.smaugslair.thitracker.ui.friends;
 
-import com.smaugslair.thitracker.data.user.FriendshipRepository;
-import com.smaugslair.thitracker.data.user.User;
-import com.smaugslair.thitracker.data.user.UserRepository;
 import com.smaugslair.thitracker.ui.MainView;
 import com.smaugslair.thitracker.util.RepoService;
-import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.combobox.ComboBox;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.splitlayout.SplitLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import org.apache.commons.lang3.StringUtils;
-import org.vaadin.addons.searchbox.SearchBox;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @PageTitle("Friends Page")
 @Route(value = "friends", layout = MainView.class)
